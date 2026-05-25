8ceec09cf4c6147382244c8d33c7cb15c94e711d
Rupesh <narayan.rupesh@gmail.com>
2026-05-25 19:53:36 +0530

    First working version with search, filter, sort

---
+++ app.py
@@ -0,0 +1,36 @@
+# app.py
+import json
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# book data
+with open('books.json') as f:
+     books = json.load(f)
+
+# Health check
+@app.route('/health')
+def health():
+    return jsonify({"status": "ok"})
+
+# Get all books
+@app.route('/books')
+def get_books():
+    query_title = request.args.get('title','').lower()
+    query_genre = request.args.get('genre','').lower()
+    sort_by = request.args.get('sort','')
+
+    results = books
+    
+    if query_title:
+       results = [b for b in results if query_title in b['title'].lower()]
+    if query_genre:
+       results = [b for b in results if query_genre == b['genre'].lower()]
+
+    if sort_by == 'rating':
+       results = sorted(results, key = lambda b:b['rating'], reverse = True)
+
+    return jsonify(results)
+
+if __name__ == '__main__':
+   app.run(port=8080, debug=True)
